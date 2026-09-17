040a08216602e6c5c497728061c0108166306070
Aousaja <m.aousaja@gmail.com>
2026-09-17 13:29:36 +0500

    Add sidebar items for navigation

---
+++ src/components/Sidebar/SidebarItems.js
@@ -0,0 +1,54 @@
+export const sidebarItems = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Sessions",
+    children: [
+      { title: "Session 01 — Getting Started with SQL", path: "/sessions/session-01" },
+      { title: "Session 02 — Basic Building Blocks in SQL", path: "/sessions/session-02" },
+      { title: "Session 03 — SQL", path: "/sessions/session-03" },
+    ],
+  },
+  //{
+  //  title: "Exercises",
+  //  children: [
+  //    { title: "Session 01 Exercises", path: "/exercises/session-01" },
+  //    { title: "Session 04 Exercises", path: "/exercises/session-04" },
+  //  ],
+  //},
+];
+
+
+
+
+
+// export const sidebarItems = [
+//   {
+//     title: "Home",
+//     path: "/",
+//   },
+//   {
+//     title: "Sessions",
+//     children: [
+//       { title: "Session 01 — C# Fundamentals", path: "/sessions/session-01" },
+//       { title: "Session 02 — Variables & Data Types", path: "/sessions/session-02" },
+//       { title: "Session 03 — Conditions", path: "/sessions/session-03" },
+//       { title: "Session 04 — Loops & Iterations", path: "/sessions/session-04" },
+//       { title: "Session 05 — Methods, Parameters & Recursion", path: "/sessions/session-05" },
+//       { title: "Session 06 — Arrays & Strings", path: "/sessions/session-06" },
+//       { title: "Session 07 — Advanced Parameters, Tuples & Exception Handling", path: "/sessions/session-07" },
+//       { title: "Session 08 - Classes, Objects, Fields, Properties, Constructors & Encapsulation", path: "/sessions/session-08" },
+//       { title: "Session 09 - Inheritance, Method Overriding & protected", path: "/sessions/session-09" },
+//       // Add remaining sessions here
+//     ],
+//   },
+//   //{
+//     //title: "Exercises",
+//     //children: [
+//     // { title: "Session 01 Exercises", path: "/exercises/session-01" },
+//     // { title: "Session 04 Exercises", path: "/exercises/session-04" },
+//     //],
+//   //},
+// ];
