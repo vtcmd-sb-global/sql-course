80285e398eb8dded07f8721ddecd315656dd8b3e
Aousaja <m.aousaja@gmail.com>
2026-09-18 16:45:12 +0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import CustomLayout from '@site/src/components/Layout/Layout';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
 import Homepage from '@site/src/components/Homepage/home';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
-  
   return (
     <Layout
-      title={`${siteConfig.title}`}
+      title="SQL Server The Definitive Guide For Bginners"
       description="SQL Server course from beginner to advanced">
       <main style={{padding: '2rem', maxWidth: 800, margin: '0 auto'}}>
         <h1>{siteConfig.title}</h1>
