d2f94e32111d0fc58d1ac3d9ee519fe8b876b813
Aousaja <m.aousaja@gmail.com>
2026-09-18 16:46:45 +0500

    Replace main content with CustomLayout and Homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import CustomLayout from '@site/src/components/Layout/Layout';
-
 import Homepage from '@site/src/components/Homepage/home';
 
 export default function Home() {
@@ -9,15 +8,11 @@ export default function Home() {
     <Layout
       title="SQL Server The Definitive Guide For Bginners"
       description="SQL Server course from beginner to advanced">
-      <main style={{padding: '2rem', maxWidth: 800, margin: '0 auto'}}>
-        <h1>{siteConfig.title}</h1>
-        <p>{siteConfig.tagline}</p>
-        <p>
-          <Link className="button button--primary button--lg" to="/sessions/session-01">
-            Start with Session 1
-          </Link>
-        </p>
-      </main>
+        {/* <Homepage /> */}
+
+      <CustomLayout>
+        <Homepage />
+      </CustomLayout>
     </Layout>
   );
 }
